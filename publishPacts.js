acb92c5316c1c1308fdb905019a1e8dca5915e4c
LayMui <laymui.toh@taiger.com>
2020-07-27 14:37:00 +0800

    Fixing consumer version using gitsha and branch

--- publishPacts.js
+++ publishPacts.js
@@ -1,7 +1,7 @@
 
 let publisher = require("@pact-foundation/pact-node")
 let path = require("path")
-//const { exec } = require("child_process");
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
@@ -10,47 +10,39 @@ if (process.env.CI !== "true") {
   process.exit(0)
 }
 
-
-
-async function getGitSha() {
-  const { stdout, stderr } = await exec('git rev-parse HEAD');
-  console.log('stdout:', stdout);
-  console.error('stderr:', stderr);
-  return stdout;
-}
-
-let gitSha;
-getGitSha().then((gitSha)  => {
-  console.log('GIT SHA:' + gitSha);
-});
-
-
-async function getBranch() {
+const getBranch = async () => {
   const { stdout, stderr } = await exec('git rev-parse --abbrev-ref HEAD');
-  console.log('stdout:', stdout);
-  console.error('stderr:', stderr);
-  return stdout;
+  return stdout.replace('\n', '');
 }
 
-getBranch().then((brname)  => {
-  console.log('brname: ' + brname);
-  return brname.replace('\n','');
-}).then(brname => {
-  let opts = {
-    providerBaseUrl: "http://localhost:8082",
-    pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
-    pactBroker: process.env.PACT_BROKER_URL,
-    pactBrokerUrl: process.env.PACT_BROKER_TOKEN,
-    check_for_potential_duplicate_pacticipant_names: "false",
-    consumerVersion:  "2.0.0",
-    //consumerVersion: gitSha,
-    tags: brname,
-  }
+const getGitShaBranch = async () => {
+  const { stdout, stderr } = await exec('git rev-parse HEAD');
+  getBranch().then((branch) => {
+    console.log('BR: ' + stdout.replace('\n', '') + ' ' + branch);
+    return stdout.replace('\n', '') + ' ' + branch;
+  }).then((combination) => {
+    console.log('COMB: ' + combination);
+    return combination;
+  })
 
-  publisher.publishPacts(opts)
-});
+}
 
-
-
-
+getGitShaBranch().then((gitsha_brname)  => {
+    console.log('gitsha_brname: ' + gitsha_brname);
+    return gitsha_brname;
+  }).then(gitsha_brname => {
+    let opts = {
+      providerBaseUrl: "http://localhost:8082",
+      pactFilesOrDirs: [path.resolve(process.cwd(), "pacts")],
+      pactBroker: process.env.PACT_BROKER_URL,
+      pactBrokerUrl: process.env.PACT_BROKER_TOKEN,
+      check_for_potential_duplicate_pacticipant_names: "false",
+      consumerVersion:  "2.0.0",
+      //consumerVersion: gitSha,
+      tags: gitsha_brname,
+    }
+  
+    publisher.publishPacts(opts)
+  });
+  
 
